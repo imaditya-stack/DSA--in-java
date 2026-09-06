a2bf24c7ad767648cfd8537e6e8e5cddea41734c
ADITYA KUMAR <imaditya.cse@gmail.com>
2026-09-06 07:21:06 +0530

    Time: 52 ms (94.44%), Space: 105.5 MB (58.54%) - LeetHub

--- 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.java
+++ 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.java
@@ -1,28 +1,53 @@
 class Solution {
     public int countDistinctIntegers(int[] nums) {
-        
-        int[] res = new int[2*nums.length];
 
-        for(int i = 0 ; i< nums.length ; i++){
-            res[i] = nums[i];
-        }
-
-        for(int i = nums.length ; i< res.length ; i++){
-            int num = nums[i - nums.length];
-            int rev = 0;
-            while(num > 0){
-                int rem = num %10;
-                rev = (rev * 10) +rem;
-                num /=10;
-            }
-            res[i] = rev;
-        }
         HashSet<Integer> set = new HashSet<>();
-        for(int j = 0 ; j< res.length ; j++){
-            set.add(res[j]);
+
+        int n = nums.length;
+        for(int i = 0 ; i<n ; i++){
+            set.add(nums[i]);
+            int rev = 0;
+            while(nums[i] > 0){
+                int rem = nums[i] % 10;
+                rev = (rev * 10) +rem;
+                nums[i] /= 10;
+            }
+            set.add(rev);
         }
 
         return set.size();
 
     }
 }
+
+
+
+
+// class Solution {
+//     public int countDistinctIntegers(int[] nums) {
+        
+//         int[] res = new int[2*nums.length];
+
+//         for(int i = 0 ; i< nums.length ; i++){
+//             res[i] = nums[i];
+//         }
+
+//         for(int i = nums.length ; i< res.length ; i++){
+//             int num = nums[i - nums.length];
+//             int rev = 0;
+//             while(num > 0){
+//                 int rem = num %10;
+//                 rev = (rev * 10) +rem;
+//                 num /=10;
+//             }
+//             res[i] = rev;
+//         }
+//         HashSet<Integer> set = new HashSet<>();
+//         for(int j = 0 ; j< res.length ; j++){
+//             set.add(res[j]);
+//         }
+
+//         return set.size();
+
+//     }
+// }
